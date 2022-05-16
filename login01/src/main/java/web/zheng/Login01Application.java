3fc8df9c29be297646e0dd627c69078d1aaaabb0
zheng-yufeng <z418589316@gmail.com>
2022-05-16 10:24:41 +0900

    first update

---
+++ login01/src/main/java/web/zheng/Login01Application.java
@@ -0,0 +1,13 @@
+package web.zheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Login01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Login01Application.class, args);
+	}
+
+}
---
+++ login01/src/test/java/web/zheng/Login01ApplicationTests.java
@@ -0,0 +1,13 @@
+package web.zheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Login01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
